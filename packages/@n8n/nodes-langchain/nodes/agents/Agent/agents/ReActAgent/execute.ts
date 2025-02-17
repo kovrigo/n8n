5646c86ce81215065d11cc387620b06d72e4ecd4
Your Name <your.email@example.com>
2025-02-17 12:49:25 +0400

    fix(ReAct Agent): Escape single quote in error message

--- packages/@n8n/nodes-langchain/nodes/agents/Agent/agents/ReActAgent/execute.ts
+++ packages/@n8n/nodes-langchain/nodes/agents/Agent/agents/ReActAgent/execute.ts
@@ -98,7 +98,7 @@ export async function reActAgentAgentExecute(
 			}
 
 			if (input === undefined) {
-				throw new NodeOperationError(this.getNode(), 'The 'text' parameter is empty.');
+				throw new NodeOperationError(this.getNode(), 'The \'text\' parameter is empty.');
 			}
 
 			if (prompt) {
